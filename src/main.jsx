7b7f661c83aaa6b3fdf4c4593dab70b4559eb45a
Sean R Reid <sean@torchcodelab.com>
2023-10-02 14:51:02 -0400

    In class walkthrough

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import App from './App.jsx';
+import Root, { loader as rootLoader } from './routes/root';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
+import './index.css';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        loader: rootLoader,
+        children: [
+            {
+                path: 'contacts/:contactId',
+                element: <Contact />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
