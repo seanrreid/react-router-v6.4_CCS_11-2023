57514e26e97a7a0eabd4865e62134ac2d401d8d3
Sean R Reid <sean@torchcodelab.com>
2023-10-02 15:18:16 -0400

    Edits through Mutation Discussion

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // import App from './App.jsx';
-import Root, { loader as rootLoader } from './routes/root';
+import Root, {
+    loader as rootLoader,
+    action as rootAction,
+} from './routes/root';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit'
 import './index.css';
 
 const router = createBrowserRouter([
@@ -13,10 +17,18 @@ const router = createBrowserRouter([
         element: <Root />,
         errorElement: <ErrorPage />,
         loader: rootLoader,
+        action: rootAction,
         children: [
             {
                 path: 'contacts/:contactId',
                 element: <Contact />,
+                loader: contactLoader,
+            },
+            {
+                path: 'contacts/:contactId/edit',
+                element: <EditContact />,
+                loader: contactLoader,
+                action: editAction
             },
         ],
     },
